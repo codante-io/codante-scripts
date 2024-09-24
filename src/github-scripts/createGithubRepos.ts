80f787b8378584cb528e77e4ba9f4aac7d6920c0
Roberto Cestari <robertotcestari@gmail.com>
2024-09-24 14:19:49 -0300

    update inquirer

--- src/github-scripts/createGithubRepos.ts
+++ src/github-scripts/createGithubRepos.ts
@@ -1,32 +1,25 @@
 import chalk from 'chalk';
 import { handle } from './lib/createRepo';
 import inquirer from 'inquirer';
+import { confirm } from '@inquirer/prompts';
 
 export async function createGithubRepos() {
-  const REPO_NAME = 'graficos-com-shadcn-e-next-js'; // sem o "mp-"
-  const DESCRIPTION =
-    'Muitas vezes gráficos são a melhor forma de "enxergarmos" os dados. Neste mini projeto, vamos criar gráficos dinâmicos com shadcn, Next.js e Recharts.';
-  const URL_DO_MINI_PROJETO =
-    'https://codante.io/mini-projetos/graficos-com-shadcn-e-next-js';
+  const REPO_NAME = 'testes-1'; // sem o "mp-"
+  const DESCRIPTION = 'Teste 1.';
+  const URL_DO_MINI_PROJETO = 'https://codante.io/mini-projetos/testes-1';
 
   console.log(chalk.blue(`Mini Projeto ${REPO_NAME}...`));
   console.log(chalk.blue(`Descrição: ${DESCRIPTION}...`));
   console.log(chalk.blue(`URL: ${URL_DO_MINI_PROJETO}...`));
 
-  await inquirer
-    .prompt([
-      {
-        type: 'confirm',
-        name: 'confirm',
-        message: `Deseja continuar?`,
-      },
-    ])
-    .then((answers) => {
-      if (!answers.confirm) {
-        console.log('Ok, script cancelado.');
-        process.exit();
-      }
-    });
+  const answer = await confirm({
+    message:
+      'Você está prestes a iniciar o script de criação de repos no Github. As informações estão corretas?',
+  });
+  if (!answer) {
+    console.log('Ok, script cancelado.');
+    process.exit();
+  }
 
   // Principal
   await handle(`mp-${REPO_NAME}`, DESCRIPTION, URL_DO_MINI_PROJETO);
